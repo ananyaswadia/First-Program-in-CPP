c4be0fa95522dae1bae91fe86ff69d1862f65e80
Ananya Swadia <ananya.swadia@gmail.com>
2025-07-24 10:17:31 +0530

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,3 +4,14 @@ using namespace std;
 int main(){
     cout<<"Hello world!"
 }
+
+//OUTPUT : 
+
+//Hello World!
+
+/*Enter first number: 5
+Enter second number: 5
+Addition of 5 and 5 is: 10
+Subtraction of 5 and 5 is: 0
+Multiplication of 5 and 5 is: 25
+Division of 5 and 5 is: 1 */
