dc033e7d46daf76f02be719661966da2cc0cdd1c
Ananya Swadia <ananya.swadia@gmail.com>
2025-07-24 10:15:21 +0530

    Add files via upload

---
+++ simple_calculator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+
+    cout<<"Enter first number: ";
+    cin>>a;
+
+    cout<<"Enter second number: ";
+    cin>>b;
+
+    cout<<"Addition of "<<a<<" and "<<b<<" is: "<<a+b<<endl;
+    cout<<"Subtraction of "<<a<<" and "<<b<<" is: "<<a-b<<endl;
+    cout<<"Multiplication of "<<a<<" and "<<b<<" is: "<<a*b<<endl;
+    cout<<"Division of "<<a<<" and "<<b<<" is: "<<a/b<<endl;
+    
+    return 0;
+}
