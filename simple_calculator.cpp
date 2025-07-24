565c00d41f23d6b554f96b675153d9db51105f6c
Ananya Swadia <ananya.swadia@gmail.com>
2025-07-24 10:19:22 +0530

    Update simple_calculator.cpp

--- simple_calculator.cpp
+++ simple_calculator.cpp
@@ -17,3 +17,10 @@ int main(){
     
     return 0;
 }
+
+/*Enter first number: 5
+Enter second number: 5
+Addition of 5 and 5 is: 10
+Subtraction of 5 and 5 is: 0
+Multiplication of 5 and 5 is: 25
+Division of 5 and 5 is: 1 */
